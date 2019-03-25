65c4358d58061dac66481fc702420716923143e2
TomTurbo2016 <gemology@live.at>
2019-03-25 12:46:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,97 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+import fnmatch
+from flask import Flask, request, redirect, url_for, render_template, session
+from werkzeug import secure_filename
+from uuid import uuid4
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+app = Flask(__name__, static_url_path='/assets')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.secret_key = os.urandom(13)
+
+
+UPLOAD_FOLDER = '/assets/userUploadImages'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'JPG', 'JPEG'])
+
+
+def deleteSpecificFilesInDir():
+    filelist = [ f for f in os.listdir(UPLOAD_FOLDER) if f.endswith(".jpg") or f.endswith(".jpeg") or f.endswith(".JPG") or f.endswith(".JPEG") ]
+    for f in filelist:
+        if fnmatch.fnmatch(f, str(session['randInt']) + 'oT-Ti' + '*') or fnmatch.fnmatch(f, 'out_' + str(session['randInt']) + 'oT-Ti' + '*') or fnmatch.fnmatch(f, 'out_big_' + str(session['randInt']) + '*'):
+            os.remove(os.path.join(UPLOAD_FOLDER, f))
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if session.get('counter') == True:
+        session['counter'] += 1
+    else:
+        session['counter'] = 0
+    if request.method == 'POST':
+        session['counter'] += 1
+        if 'file' not in request.files:
+            return redirect(url_for('file_upload_error_nofile'))
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            session['randInt'] = str(uuid4())
+            randInt = str(session['randInt'])
+            filename = randInt + 'oT-Ti' + filename
+            session['img_filename'] = filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('NEW_uploaded_file', filename=filename))
+        else:
+            return redirect(url_for('file_upload_error_nojpg'))
+    else:
+        if session['counter'] >= 1:
+            deleteSpecificFilesInDir()
+        return render_template('main_page.html')
+
+
+##Error-Messages:
+@app.route('/FILE_UPLOAD_ERROR_NoFileSelected')
+def file_upload_error_nofile():
+    return '''
+    <!doctype html>
+    <title>Upload File ERROR - No File selected</title>
+    <h1>No file selected</h1>
+    <button onclick="goBack()">Go Back</button>
+    <script>
+    function goBack() {
+    window.location.href='https://gexvo.onrender.com';
+    }
+    </script>
+    '''
+
+@app.route('/FILE_UPLOAD_ERROR_NoJPGformat')
+def file_upload_error_nojpg():
+    return '''
+    <!doctype html>
+    <title>Upload File ERROR - No jpg Format</title>
+    <h1>File needs to end with .jpg</h1>
+    <button onclick="goBack()">Go Back</button>
+    <script>
+    function goBack() {
+    window.location.href='https://gexvo.onrender.com';
+    }
+    </script>
+    '''
+
+@app.route('/GENERALERROR')
+def generalError():
+    return '''
+    <!doctype html>
+    <title>ERROR</title>
+    <h1>Something went wrong!</h1>
+    <button onclick="goBack()">Go Back</button>
+    <script>
+    function goBack() {
+    window.location.href='https://gexvo.onrender.com';
+    }
+    </script>
+    '''
+
