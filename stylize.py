22388f1484d1275dd69023c4b79538d5a7be152f
TomTurbo2016 <gemology@live.at>
2019-03-27 12:32:40 +0100

    Update stylize.py

--- stylize.py
+++ stylize.py
@@ -144,7 +144,7 @@ def stylize(_pathInputPic, _scaleFactor, _pathOutputPic, _model):
 
 
 async def doWork(pathInputPic, pathOutputPic, nameStyle, pathModel, scaleFactor):
-    stylize(pathInputPic, 1, pathOutputPic, pathModel + '/' + nameStyle + '.pth')
+    stylize(pathInputPic, 0.5, pathOutputPic, pathModel + '/' + nameStyle + '.pth')
 
 
 def main(pathInputPic, pathOutputPic, nameStyle, pathModel, scaleFactor):
