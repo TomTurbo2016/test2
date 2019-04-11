a0c448fcf10b789a664ecf7e6c1c44f8cf54fee2
TomTurbo2016 <gemology@live.at>
2019-04-11 14:43:54 +0200

    Update imageResize2.py

--- imageResize2.py
+++ imageResize2.py
@@ -1,15 +1,9 @@
-import cv2
-
-def main(_pathInputPic, _downscale):
-    oriimg = cv2.imread(_pathInputPic)
-    height, width, depth = oriimg.shape
-    baseWidth = width / 3 * _downscale
-    imgScale = baseWidth/width
-    newX,newY = oriimg.shape[1]*imgScale, oriimg.shape[0]*imgScale
-    oriimg = cv2.resize(oriimg, (int(newX), int(newY)), interpolation=cv2.INTER_AREA)
-    cv2.imwrite(_pathInputPic, oriimg)
+from cv2 import imread, resize, imwrite, INTER_AREA
+from PIL import Image
+import numpy as np
 
 
-##---------------------------------------------------------------------------------------------------------->
-#Preferable interpolation methods are cv.INTER_AREA for shrinking and cv.INTER_CUBIC(slow) & cv.INTER_LINEAR
-#for zooming. By default, interpolation method used is cv.INTER_LINEAR for all resizing purposes.
+def main2(_img):
+    width, height = _img.size
+    pic = _img.resize((int(width / 4 * 3), int(height / 4 * 3)), Image.LANCZOS)
+    return np.array(pic)
