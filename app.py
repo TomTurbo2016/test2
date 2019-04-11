7a59f55b49068756464da70f07d804641f73ca69
TomTurbo2016 <gemology@live.at>
2019-04-11 15:37:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, redirect, url_for, session #, render_template
+from flask import Flask, request, redirect, url_for, session
 from random import randint
 import requests
 import stylize2
