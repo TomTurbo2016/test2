4cb69a291c62e2e97850efc71763ee2427cfce3e
TomTurbo2016 <gemology@live.at>
2019-04-11 14:44:20 +0200

    Update imageResize.py

--- imageResize.py
+++ imageResize.py
@@ -2,21 +2,6 @@ from cv2 import imread, imdecode, resize, imwrite, IMREAD_COLOR, INTER_AREA
 import numpy as np
 
 
-def main(baseWidth, pathInputPic):
-    oriimg = imread(pathInputPic)
-    height, width, depth = oriimg.shape
-    if width > baseWidth or height > baseWidth:
-        if height >= width:
-            imgScale = baseWidth/width
-            newX,newY = oriimg.shape[1]*imgScale, oriimg.shape[0]*imgScale
-            oriimg = resize(oriimg, (int(newX), int(newY)), interpolation=INTER_AREA)
-        else:
-            imgScale = baseWidth/height
-            newX,newY = oriimg.shape[1]*imgScale, oriimg.shape[0]*imgScale
-            oriimg = resize(oriimg, (int(newX), int(newY)), interpolation=INTER_AREA)
-    imwrite(pathInputPic, oriimg)
-
-
 def main2(baseWidth, imageBytes):
     npArr = np.asarray(bytearray(imageBytes.read()), dtype=np.uint8)
     oriimg = imdecode(npArr, IMREAD_COLOR)
