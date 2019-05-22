3e647b44580849e25f1a408899565bb5b8a3b056
TomTurbo2016 <gemology@live.at>
2019-05-22 14:41:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ async def ShowPic():
 				ioFile.write(base64.b64decode(openBase64StringFromFile(PATH_TO_BASE64_TXT_FOLDER + url_id + '.txt', prefix + url_id)))
 				ioFile.seek(0)
 				selectedStyle = (await request.form)['stylize']
-				create_job(selectedStyle, ioFile, url_id)
+				await create_job(selectedStyle, ioFile, url_id)
 				#asyncio.get_running_loop().run_in_executor(None, cpu_background_task(selectedStyle, ioFile, url_id))
 				prefix = 'S' #Style
 				return ("<!DOCTYPE html>"
