eecccc5f636e62caf5859a6bcbd7a455dbcede4c
TomTurbo2016 <gemology@live.at>
2019-06-05 16:59:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import datetime
 #import watermark
 
 ####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~To-Be-Edited~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
-START_URL = 'https://testquart.onrender.com/'
+START_URL = 'https://testflask.onrender.com/'
 PATH_TO_BASE64_TXT_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/base64strings/')
 PATH_TO_STYLE_FILES = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/styleModels/')
 PATH_TO_SCALE_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/upscaleModel/')
